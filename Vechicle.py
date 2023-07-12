834cae37967fa2dcf63eb915b0e9dcbe2683d1ee
snp131 <snp131@scarletmail.rutgers.edu>
2023-07-12 16:07:04 -0400

    Co-authored-by: faze77 <faze77@users.noreply.github.com> Co-authored-by: bhansea <bhansea@users.noreply.github.com>

---
+++ Vechicle.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    # car details
+    carModel = ""
+    carMake = ""
+    carYear = 0
+    carID = 1234
+
+    # car trunk dimensions
+    trunkLength = 0
+    trunkWidth = 0
+    trunkHeight = 0
+
+    # weight limit in cubic feet
+    carThreshold = 0
+
+
+    
+
+
